dc7e53040b7056ba0de0b0ca85a29c139ad7b198
dmackca <dmack.ca@gmail.com>
2020-08-25 20:50:47 -0700

    add error handling

--- src/index.js
+++ src/index.js
@@ -3,18 +3,48 @@ const Eris = require('eris');
 
 const bot = new Eris(process.env.BOT_TOKEN);
 
-bot.on('ready', () => { // When the bot is ready
-    console.log('Ready!'); // Log "Ready!"
+const DEBUG_CHANNEL_ID = '746568285323133001';
+
+function sendErrorMessage() {
+    return bot.createMessage(DEBUG_CHANNEL_ID, '<@246852441696370689> go check the error log!');
+}
+
+// When the bot is ready
+bot.on('ready', () => {
+    console.log('Ready!');
 });
 
-bot.on('messageCreate', async (msg) => { // When a message is created
+// handle error-ish events
+bot.on('disconnect', async (msg) => {
+    console.error('disconnect happened', msg);
+    sendErrorMessage(bot);
+});
+
+bot.on('error', async (err, id) => {
+    console.error('error happened', err, id);
+    sendErrorMessage(bot);
+});
+
+bot.on('shardDisconnect', async (err, id) => {
+    console.error('shardDisconnect happened', err, id);
+    sendErrorMessage(bot);
+});
+
+// When a message is created
+bot.on('messageCreate', async (msg) => {
+    // frinkiac queries
     if (msg.content.startsWith('.frink ')) {
         const frinkiacQuery = msg.content.substring(7);
-        console.log('got .frink command:', frinkiacQuery);
+        console.log('got .frink query: "%s" in channel %s.', frinkiacQuery, msg.channel.id);
 
-        const gifUrl = await gifGenerator(frinkiacQuery);
-        bot.createMessage(msg.channel.id, gifUrl);
-        console.log('sent', gifUrl);
+        try {
+            const gifUrl = await gifGenerator(frinkiacQuery);
+            bot.createMessage(msg.channel.id, gifUrl);
+            console.log('sent', gifUrl);
+        } catch (e) {
+            console.error('no gif result found');
+            msg.addReaction('🤷‍♀️');
+        }
     }
 });
 
